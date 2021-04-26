05431f4d13775f4a6d2bcfb9c3e33efb8d8dfe57
SarahSibert <mail@sarahsibert.com>
2021-04-26 15:21:48 +0100

    Add index and package.json

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = require("tailwindcss/plugin")(
+    function ({}) {
+        }, 
+        { 
+        theme: {
+            extend: {
+                colors: {
+                    "astrotomic": "#7E59C5",
+                    "moonlight": "#001543",
+                    "night": "#001425",
+                    "mit": "#5461C8",
+                    "treeware": "#318D3A",
+                    "larabelles": "#F09491",
+                },
+            }
+        }
+    }
+);
